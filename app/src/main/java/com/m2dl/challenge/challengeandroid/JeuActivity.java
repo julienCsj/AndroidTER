81c06917d4bd6caeec9fdc6f70052c85329b8a6d
superkebab <felgines.elliot@gmail.com>
2016-01-28 08:29:34 +0100

    Structure projet

---
+++ app/src/main/java/com/m2dl/challenge/challengeandroid/JeuActivity.java
@@ -0,0 +1,13 @@
+package com.m2dl.challenge.challengeandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JeuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeu);
+    }
+}
---
+++ app/src/main/java/com/m2dl/challenge/challengeandroid/Model/Objet.java
@@ -0,0 +1,8 @@
+package com.m2dl.challenge.challengeandroid.Model;
+
+/**
+ * Created by Elliot on 28/01/2016.
+ */
+// Les objets qui tombent du ciel
+public class Objet {
+}
